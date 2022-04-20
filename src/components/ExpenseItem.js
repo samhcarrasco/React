3bad9ae4032e6136c9fee4bc1b33cecd9ef15539
samhcarrasco <samhcarrasco@gmail.com>
2022-04-20 18:57:51 -0400

    added css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,13 @@
+import './ExpenseItem.css';
 function ExpenseItem() {
-  return (<div>
-    <div>March 28th 2001</div>
-    <div>
-      <h2>Car Insurance</h2>
-    <div>$294.67</div>
+  return (
+    <div className='expense-item'>
+      <div >March 28th 2021</div>
+      <div className='expense-item__description'>
+        <h2>Car Insurance</h2>
+        <div className='expense-item__price'>$294.67</div>
+      </div>
     </div>
-  </div>);
+  );
 }
+export default ExpenseItem;
