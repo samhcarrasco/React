d1c8efab0198176acae4f943ded91c930a8524d6
samhcarrasco <samhcarrasco@gmail.com>
2022-04-20 19:30:03 -0400

    remove DS store

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <ExpenseItem/>
+      <ExpenseItem/>
+      <ExpenseItem/>
+      <ExpenseItem/>
+      <ExpenseItem/>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import './ExpenseItem.css';
 function ExpenseItem() {
+  const expenseDate= new Date(2021, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
   return (
     <div className='expense-item'>
-      <div >March 28th 2021</div>
+      {/*.toISOString() turns date objects into text */}
+      <div >{expenseDate.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
     </div>
   );
