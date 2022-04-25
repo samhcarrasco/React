c73489eea5f8ed98efe081a178a72279e35116e0
samhcarrasco <samhcarrasco@gmail.com>
2022-04-25 11:15:38 -0700

    separated expense component from expense date commponent/added styling to expense date component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,14 @@
 import './ExpenseItem.css';
-function ExpenseItem() {
-  const expenseDate= new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
+import ExpenseDate from './ExpenseDate'
+function ExpenseItem(props) {
+
   return (
     <div className='expense-item'>
       {/*.toISOString() turns date objects into text */}
-      <div >{expenseDate.toISOString()}</div>
+      <ExpenseDate date = {props.date}/>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
