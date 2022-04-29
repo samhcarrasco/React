a8cbff9f828f360ac9a44042df852eecdeca35be
samhcarrasco <samhcarrasco@gmail.com>
2022-04-29 10:23:03 -0700

    added styling to expenses

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
   ];
   return (
     <div>
+      <h2>Let's get started!</h2>
     <Expenses expenses = {expenses}/>
   </div>
   );
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,10 +1,10 @@
 import ExpenseItem from './ExpenseItem';
-
+import './Expenses.css'
 const Expenses = (props) =>{
 
   return(
-     <div>
-    <h2>Let's get started!</h2>
+     <div className='expenses'>
+    
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
